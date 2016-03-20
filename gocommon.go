7a35c6db8ecf14436a692300076a8a67bfd00ed2
Casey Bisson <casey.bisson@joyent.com>
2016-03-20 12:12:50 -0700

    add license notice to *.go files

--- gocommon.go
+++ gocommon.go
@@ -9,10 +9,14 @@ The gocommon package is structured as follow:
 	- gocommon/jpc. This package provides common structures and functions across packages.
 	- gocommon/testing. Testing Suite for local testing.
 
-Licensed under Mozilla Public License Version 2.
-
 Copyright (c) 2013 Joyent Inc.
 Written by Daniele Stroppa <daniele.stroppa@joyent.com>
 
 */
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package gocommon
