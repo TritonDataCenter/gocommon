04878964200921ca3a691f4864feb7bdc2abb0a8
Casey Bisson <casey.bisson@joyent.com>
2016-03-20 11:46:00 -0700

    joyent/gocommon#6 relicense as MPLv2

--- gocommon.go
+++ gocommon.go
@@ -9,7 +9,7 @@ The gocommon package is structured as follow:
 	- gocommon/jpc. This package provides common structures and functions across packages.
 	- gocommon/testing. Testing Suite for local testing.
 
-Licensed under LGPL v3.
+Licensed under Mozilla Public License Version 2.
 
 Copyright (c) 2013 Joyent Inc.
 Written by Daniele Stroppa <daniele.stroppa@joyent.com>
